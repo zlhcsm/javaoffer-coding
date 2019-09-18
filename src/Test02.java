124665931650ce317b586f3f8a9328fca8c8ae23
zlhcsm <522496405@qq.com>
2019-09-18 21:41:42 +0800

    Update Test02.java

--- src/Test02.java
+++ src/Test02.java
@@ -1,16 +1,55 @@
-int maxsequence(int arr[], int len)
-{
-    int max = arr[0]; //初始化最大值为第一个元素
-    for (int i=0; i<len; i++) {
-        int sum = 0; //sum必须清零
-        for (int j=i; j<len; j++) { //从位置i开始计算从i开始的最大连续子序列和的大小，如果大于max，则更新max。
-            sum += arr[j]; 
-            if (sum > max)
-                max = sum;
-        }
-    }
-    return max;
+import java.util.Scanner;
 
-————————————————
-版权声明：本文为CSDN博主「石锅拌饭」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/sgbfblog/article/details/8032464
+public class Main3 {
+
+ @SuppressWarnings("resource")
+ public static void main(String[] args) {
+  Scanner scanner = new Scanner(System.in);
+  String string = scanner.nextLine();
+  if(string == null || string.length() <= 2)
+  {
+   System.out.println("0");
+   return;
+   
+  }
+  string = string.substring(1, string.length() - 1);
+  
+  if(string == null || string.length() == 0)
+  {
+   System.out.println("0");
+   return;
+  }
+  String[] strings = string.split(", ");
+  if(strings.length == 0 || strings == null)
+  {
+   System.out.println("0");
+   return ;
+  }
+  Long[] arr = new Long[strings.length];
+  for(int i = 0; i < arr.length; i++)
+  {
+   arr[i] = Long.valueOf(strings[i]);
+   //System.out.println(arr[i]);
+  }
+  if(arr == null || arr.length == 0){
+   System.out.println("0");
+   return;
+  }
+  long greateResult = Long.MIN_VALUE;
+  long tempResult = 0;
+  for(int i = 0; i < arr.length; i++){
+   if(tempResult <= 0){
+    tempResult = arr[i];
+   }
+   else
+   {
+    tempResult += arr[i];
+   }
+   if(greateResult < tempResult)
+   {
+    greateResult = tempResult;
+   }
+  }
+  System.out.println(greateResult);
+ }
+}
