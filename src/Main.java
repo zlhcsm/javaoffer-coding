77b8056c293eb99a8a8fedb7a846c86a4193d511
nhl220 <522496405@qq.com>
2019-09-18 21:14:28 +0800

    first comment

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,25 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        while (scan.hasNext()) {
+
+        while (scan.hasNext()){
             int n = scan.nextInt();
-            String[] arr = new String[n];
 
+            int res = 0;
             for (int i = 0; i < n; i++) {
-                arr[i] = scan.next();
+                int num = scan.nextInt();
+                res = res ^ num;
             }
+            System.out.println(res);
 
-            createTree(arr);
         }
+
     }
 
-    public static class TreeNode {
-
-        private TreeNode left;
-        private TreeNode right;
-
-        private int value;
-
-        public TreeNode(int value) {
-
-            this.value = value;
-        }
-    }
-
-    public static TreeNode createTree(String[] arr) {
-        if (arr == null)
-            return null;
-        TreeNode[] nodeArray = new TreeNode[arr.length];
-        for (int i = 0; i <= arr.length - 1; i++) {
-            if (!arr[i].equals("#")) {
-                if (nodeArray[i] == null) {
-                    nodeArray[i] = new TreeNode(Integer.valueOf(arr[i]));
-                }
-                int index = 2 * i + 1;
-                if (index < arr.length && !arr[index].equals("#")) {
-                    nodeArray[index] = new TreeNode(Integer.valueOf(arr[index]));
-                    nodeArray[i].left = nodeArray[index];
-                } else {
-                    nodeArray[i].left = null;
-                }
-                index++;
-                if (index < arr.length && !arr[index].equals("#")) {
-                    nodeArray[index] = new TreeNode(Integer.valueOf(arr[index]));
-                    nodeArray[i].right = nodeArray[index];
-                } else {
-                    nodeArray[i].right = null;
-                }
-            }
-        }
-        return nodeArray[0];
-    }
 }
--- src/Test02.java
+++ src/Test02.java
@@ -1,66 +1,66 @@
-public class Test02 {
-}
-package shenxinfu;
-
-import java.util.Scanner;
-
-public class CreateTree {
-
- public static class TreeNode{
-
-  private TreeNode left;
-  private TreeNode right;
-
-  private int value;
-
-  public TreeNode(int value) {
-
-   this.value = value;
-  }
- }
- public static TreeNode createTree(String[] arr){
-  if(arr == null)
-   return null;
-  TreeNode[] nodeArray = new TreeNode[arr.length];
-  for(int i = 0; i <= arr.length - 1; i++){
-   if(!arr[i].equals("#")){
-    if(nodeArray[i] == null){
-     nodeArray[i] = new TreeNode(Integer.valueOf(arr[i]));
-    }
-    int index = 2 * i + 1;
-    if(index < arr.length && !arr[index].equals("#")){
-     nodeArray[index] = new TreeNode(Integer.valueOf(arr[index]));
-      nodeArray[i].left = nodeArray[index];
-    }else
-    {
-     nodeArray[i].left = null;
-    }
-    index++ ;
-    if(index < arr.length && !arr[index].equals("#")){
-     nodeArray[index] = new TreeNode(Integer.valueOf(arr[index]));
-     nodeArray[i].right = nodeArray[index];
-    }else
-    {
-     nodeArray[i].right = null;
-    }
-   }
-  }
-  return nodeArray[0];
- }
-public static String preOrderTravel(TreeNode node) {
-  if(node == null)
-   return "#,";
-  String res = node.value + ",";
-  res += preOrderTravel(node.left);
-  res += preOrderTravel(node.right);
-  return res;
-    }
-    @SuppressWarnings("resource")
- public static void main(String[] args) {
-  Scanner scanner = new Scanner(System.in);
-  String string = scanner.nextLine();
-  String[] strings = string.split(",");
-  TreeNode root = createTree(strings);
-  String s = preOrderTravel(root);
-  System.out.println(s.substring(0,s.length() - 1));
- }
+//public class Test02 {
+//}
+//package shenxinfu;
+//
+//import java.util.Scanner;
+//
+//public class CreateTree {
+//
+// public static class TreeNode{
+//
+//  private TreeNode left;
+//  private TreeNode right;
+//
+//  private int value;
+//
+//  public TreeNode(int value) {
+//
+//   this.value = value;
+//  }
+// }
+// public static TreeNode createTree(String[] arr){
+//  if(arr == null)
+//   return null;
+//  TreeNode[] nodeArray = new TreeNode[arr.length];
+//  for(int i = 0; i <= arr.length - 1; i++){
+//   if(!arr[i].equals("#")){
+//    if(nodeArray[i] == null){
+//     nodeArray[i] = new TreeNode(Integer.valueOf(arr[i]));
+//    }
+//    int index = 2 * i + 1;
+//    if(index < arr.length && !arr[index].equals("#")){
+//     nodeArray[index] = new TreeNode(Integer.valueOf(arr[index]));
+//      nodeArray[i].left = nodeArray[index];
+//    }else
+//    {
+//     nodeArray[i].left = null;
+//    }
+//    index++ ;
+//    if(index < arr.length && !arr[index].equals("#")){
+//     nodeArray[index] = new TreeNode(Integer.valueOf(arr[index]));
+//     nodeArray[i].right = nodeArray[index];
+//    }else
+//    {
+//     nodeArray[i].right = null;
+//    }
+//   }
+//  }
+//  return nodeArray[0];
+// }
+//public static String preOrderTravel(TreeNode node) {
+//  if(node == null)
+//   return "#,";
+//  String res = node.value + ",";
+//  res += preOrderTravel(node.left);
+//  res += preOrderTravel(node.right);
+//  return res;
+//    }
+//    @SuppressWarnings("resource")
+//// public static void main(String[] args) {
+////  Scanner scanner = new Scanner(System.in);
+////  String string = scanner.nextLine();
+////  String[] strings = string.split(",");
+////  TreeNode root = createTree(strings);
+////  String s = preOrderTravel(root);
+////  System.out.println(s.substring(0,s.length() - 1));
+//// }
